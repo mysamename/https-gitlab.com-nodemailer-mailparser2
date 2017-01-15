66f5a141efbfc569221bb8f442cba78cbdbc7c54
Andris Reinman <andris@kreata.ee>
2017-01-15 15:44:48 +0200

    working draft

--- test/pipe.js
+++ test/pipe.js
@@ -5,19 +5,47 @@ const fs = require('fs');
 const Mailparser2 = require('../lib/mailparser2.js');
 
 let parser = new Mailparser2();
-let input = fs.createReadStream(__dirname + '/eee.eml');
+let input = fs.createReadStream(__dirname + '/large_out.eml');
 
 input.pipe(parser);
 
 parser.on('headers', headers => {
-    console.log('HEADERS');
     console.log(util.inspect(headers, false, 22));
 });
 
 parser.on('data', data => {
-    console.log('DATA');
+    if (data.type === 'text') {
+        Object.keys(data).forEach(key => {
+            console.log(key);
+            console.log('----');
+            console.log(data[key]);
+        });
+    }
+
+    if (data.type === 'attachment') {
+        console.log('ATTACHMENT');
+        let size = 0;
+        Object.keys(data).forEach(key => {
+            if (typeof data[key] !== 'object' && typeof data[key] !== 'function') {
+                console.log('%s: %s', key, JSON.stringify(data[key]));
+            }
+        });
+        data.content.on('readable', () => {
+            let chunk;
+            while ((chunk = data.content.read()) !== null) {
+                size += chunk.length;
+            }
+        });
+
+        data.content.on('end', () => {
+            console.log('%s: %s B', 'size', size);
+            setTimeout(() => data.release(), 100);
+        });
+    }
+    //attachment.release();
+    //attachment.content.pipe(process.stdout);
 });
 
 parser.on('end', () => {
-    console.log('END');
+    console.log('READY');
 });
